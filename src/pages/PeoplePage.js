85a5afe9fa8d6cb0582139a8788932a705d74dbf
Daniel Alves <danielalves.ccomp@gmail.com>
2020-06-27 15:36:02 -0300

    Tratamento de erros

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Platform, Text, View, ActivityIndicator } from 'react-native';
+import { Platform, Text, View, ActivityIndicator, StyleSheet } from 'react-native';
 import PeopleList from '../components/PeopleList';
 import axios from 'axios';
 
@@ -10,6 +10,7 @@ export default class PeoplePage extends React.Component {
     this.state = {
       peoples: [],
       loading: false,
+      error: false,
     };
   }
 
@@ -17,32 +18,67 @@ export default class PeoplePage extends React.Component {
     this.setState({ loading: true });
     setTimeout(() => {
       axios
-        .get('https://randomuser.me/api/?nat=br&results=15')
+        .get('https://randomusermmm.me/api/?nat=br&results=15')
         .then(response => {
           const { results } = response.data;
           this.setState({
             peoples: results,
             loading: false,
           });
-        })
-    }, 1500)
+        }).catch(error => {
+          this.setState({ 
+            loading: false,
+            error: true,
+          })
+        });
+    }, 350)
   }
+
+  renderPage() {
+		if (this.state.loading) {
+			return <ActivityIndicator size="large" color="#6ca2f7" />;
+		}
+
+		if (this.state.error) {
+			return <Text style={styles.error}>Ops... Algo deu errado =(</Text>;
+		}
+
+		return (
+			<PeopleList
+				peoples={this.state.peoples}
+				onPressItem={pageParams => {
+					this.props.navigation.navigate('PeopleDetail', pageParams);
+				}} />
+		);
+	}
 
   render() {
     return (
-      <View>
-        {
-            this.state.loading 
-                ? <ActivityIndicator size="large" color="#142850" />
-                : null
-        }
-        <PeopleList 
-          peoples={this.state.peoples} 
-          onPressItem={pageParams => {
-            this.props.navigation.navigate('PeopleDetail', pageParams);
-          }}
-        />
+      <View style={styles.container}>
+        { this.renderPage() }
+        {/* {
+          this.state.loading 
+            ? <ActivityIndicator size="large" color="#142850" />
+            : <PeopleList 
+                peoples={this.state.peoples} 
+                onPressItem={pageParams => {
+                  this.props.navigation.navigate('PeopleDetail', pageParams);
+                }}
+              />
+        }         */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+	error: {
+		color: 'red',
+		alignSelf: 'center',
+		fontSize: 20,
+	}
+});
